48fd261f22fe18d2cf510b6f60550af56ee76013
Md Rahi <fhrfhr102030@gmail.com>
2022-11-08 18:12:55 +0600

    load services data

--- index.js
+++ index.js
@@ -12,6 +12,23 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yaqgjrz.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
+    try{
+        const serviceCollection = client.db('rapidTrip').collection('services');
+
+        app.get('/services', async(req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(err => console.error(err));
+
 app.get('/', (req, res) => {
     res.send('Rapid Trip Server is Running')
 })
